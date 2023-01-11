9efc7a72e1af799acf66bdacbead5fd3e002e55e
Dima D <dimdon88@gmail.com>
2023-01-11 11:02:07 +0700

    feat: add exersice-5 js file

---
+++ Lessons/01 Fundamentals-Part-1/exersice-5.js
@@ -0,0 +1,23 @@
+/*
+function bmiCalculator1(weight, height) {
+	const bmi = Math.round(weight / height ** 2);
+	if (bmi < 18.5) {
+		return `Your BMI is ${bmi}, so you are underweight.`;
+	} else if (bmi >= 18.5 && bmi <= 24.9) {
+		return `Your BMI is ${bmi}, so you have a normal weight.`;
+	} else {
+		return `Your BMI is ${bmi}, so you are overweight.`;
+	}
+}
+console.log(bmiCalculator1(145, 1.8));
+function isLeap(year) {
+	if (year % 400 === 0) {
+		return `Leap year.`;
+	} else if (year % 4 === 0 && year % 100 !== 0) {
+		return `Leap year.`;
+	} else {
+		return `Not leap year.`;
+	}
+}
+console.log(isLeap(44));
+*/
