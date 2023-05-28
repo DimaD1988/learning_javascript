30015073728c1a557a17b50dbb1e9711ac06a726
DimaD1988 <dimdon88@gmail.com>
2023-05-28 10:20:46 +0700

    feat: add Arrays exersice-6 js file

---
+++ Lessons/09-Arrays/exersice-6.js
@@ -0,0 +1,20 @@
+'use strict';
+///////////////////////////////////////
+// Coding Challenge #3
+
+/* 
+Rewrite the 'calcAverageHumanAge' function from the previous challenge, but this time as an arrow function, and using chaining!
+
+TEST DATA 1: [5, 2, 4, 1, 15, 8, 3]
+TEST DATA 2: [16, 6, 10, 5, 6, 1, 4]
+
+GOOD LUCK 😀
+const calcAverageHumanAge = ages =>
+ages
+.filter(i => i > 2)
+.map(a => a * 4 + 16)
+.reduce((acc, cur, i, arr) => acc + cur / arr.length, 0);
+
+console.log(calcAverageHumanAge([5, 2, 4, 1, 15, 8, 3]));
+console.log(calcAverageHumanAge([16, 6, 10, 5, 6, 1, 4]));
+*/
