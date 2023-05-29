689e8b2fd1fa1ee035583814157a7f7e28905164
DimaD1988 <dimdon88@gmail.com>
2023-05-29 11:24:06 +0700

    feat: add Arrays exersice-7 js file

---
+++ Lessons/09-Arrays/exersice-7.js
@@ -0,0 +1,16 @@
+'use strict';
+// Create pangram
+/*
+const isPangram = sentence => {
+  let lCased = sentence.toLowerCase();
+  for (let char of 'abcdefghijklmnopqrstuvwxyz') {
+    // if (lCased.indexOf(char) === -1) {
+    if (!lCased.includes(char)) {
+      return false;
+    }
+  }
+  return true;
+};
+// console.log(isPangram(`The five boxing wizards jump quickly`));
+// isPangram(`The five boxing wizards jump quickl`);
+*/
