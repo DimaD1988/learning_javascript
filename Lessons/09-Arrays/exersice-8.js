a7501a639ae8abb54d3a3110ad680aa920d693cc
DimaD1988 <dimdon88@gmail.com>
2023-05-30 12:03:48 +0700

    feat: add Array exersice-8 js file

---
+++ Lessons/09-Arrays/exersice-8.js
@@ -0,0 +1,40 @@
+'use strict';
+/*
+// Write a getCard() function which returns a random playing card Object, like:
+// {
+//   value: `K`'
+//   suit: `clubs`
+// }
+// Pick a random value from:
+//  2,3,4,5,6,7,8,9,10,J,Q,K,A
+// Pick a random suit from:
+// clubs, spades, hearts, diamonds
+// return both in a object
+
+const pick = arr => Math.floor(Math.random() * arr.length);
+
+const getCard = () => {
+  const valueArr = [
+    `2`,
+    `3`,
+    `4`,
+    `5`,
+    `6`,
+    `7`,
+    `8`,
+    `9`,
+    `10`,
+    `J`,
+    `Q`,
+    `K`,
+    `A`,
+  ];
+  const suitArr = [`clubs`, `spades`, `hearts`, `diamonds`];
+
+  return {
+    value: valueArr[pick(valueArr)],
+    suit: suitArr[pick(suitArr)],
+  };
+};
+console.log(getCard());
+*/
