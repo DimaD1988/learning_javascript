748beb5231e636dfeb3b229dd39391e4f7573e89
DimaD1988 <dimdon88@gmail.com>
2023-06-05 10:15:13 +0700

    feat: add DatesTimers exersice-1 js file

---
+++ Lessons/10-Dates & Timers/exersice-1.js
@@ -0,0 +1,56 @@
+'use strict';
+/*
+// BINARY BASE
+console.log(0.1 + 0.2);
+console.log(0.1 + 0.2 === 0.3);
+
+// CONVERSION
+console.log(Number(`24`));
+console.log(+`24`);
+
+// PARSING
+console.log(Number.parseInt(`30px`)); // will work
+console.log(Number.parseInt(`px30`)); // not work
+
+console.log(Number.parseInt(11, 2)); // Binary to decade (=3)
+console.log(Number.parseInt(`a1`, 16)); // Hex to decade (=161)
+
+console.log(Number.parseInt(`2.5rem`)); //(=2)
+console.log(Number.parseFloat(`2.5rem`)); //(=2.5)
+
+// Checking if value is number
+console.log(Number.isFinite(24)); // true
+console.log(Number.isFinite(`24`)); // false
+console.log(Number.isFinite(+`24`)); // true
+console.log(Number.isFinite(24 / 0)); // false
+*/
+/*
+console.log(Math.sqrt(2));
+console.log(8 ** (1 / 3));
+console.log(Math.max(5, 18, 23, 9, 7));
+console.log(Math.max(5, `18`, 23, 9, 7));
+console.log(Math.max(5, `18`, `23px`, 9, 7));
+*/
+
+// ROUNDING INTEGERS
+/*
+console.log(Math.trunc(-23.3)); // = -23
+console.log(Math.trunc(23.3)); // = 23
+
+console.log(Math.round(-23.3)); // = -23
+console.log(Math.round(23.3)); // = 23
+
+console.log(Math.floor(-23.3)); // = -24
+console.log(Math.floor(23.3)); // = 23
+
+console.log(Math.ceil(-23.3)); // = -23
+console.log(Math.ceil(23.3)); // = 24
+*/
+
+// ROUNDING DECIMALS
+/*
+console.log((2.7).toFixed(0)); // = `3`
+console.log((2.7).toFixed(3)); // = `2.700`
+console.log((2.345).toFixed(2)); // = `2.35`
+console.log(+(2.345).toFixed(2)); // = 2.35
+*/
