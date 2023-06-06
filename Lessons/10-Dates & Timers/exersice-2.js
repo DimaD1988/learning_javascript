b16939d2ea195feed3b271d89a3973186ad9d0c3
DimaD1988 <dimdon88@gmail.com>
2023-06-06 09:55:48 +0700

    feat: add DatesTimers exersice-2 js file

---
+++ Lessons/10-Dates & Timers/exersice-2.js
@@ -0,0 +1,26 @@
+'use strict';
+/*
+// REMINDER
+console.log(5 % 2);
+console.log(5 / 2);
+
+console.log(8 % 3);
+console.log(8 / 3);
+
+console.log(6 % 2);
+console.log(7 % 2);
+
+const diameter = 287_460_000_000;
+console.log(diameter);
+
+// BIG_INT
+console.log(Number.MAX_SAFE_INTEGER);
+console.log(Number.MIN_SAFE_INTEGER);
+
+console.log(79576737575091977867516661120248876n);
+console.log(BigInt(4535781));
+
+console.log(20n > 15);
+console.log(20n < 15);
+console.log(10n < 15);
+*/
