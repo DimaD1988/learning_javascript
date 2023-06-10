fbabd14d3a57ab50d7026ce701c302380ab191b8
DimaD1988 <dimdon88@gmail.com>
2023-06-10 10:46:36 +0700

    feat: add DatesTimers exersice-5 js file

---
+++ Lessons/10-Dates & Timers/exersice-5.js
@@ -0,0 +1,30 @@
+'use strict';
+/*
+const mesg = () => {
+  console.log(`now`);
+  setTimeout(() => console.log(`A moment later`), 1000);
+  setTimeout(() => console.log(`Two seconds later`), 2000);
+  setTimeout(() => console.log(`Three seconds later`), 3000);
+  setTimeout(() => console.log(`Four seconds later`), 4000);
+  setTimeout(() => console.log(`Five seconds later`), 5000);
+};
+mesg();
+*/
+/*
+// setTimeout function
+
+const ingredients = [`olives`, `spinach`];
+const pizzaTimer = setTimeout(
+  (ing1, ing2) => console.log(`Here is your pizza with ${ing1} and ${ing2}`),
+  3000,
+  ...ingredients
+);
+console.log(`Waiting...`);
+
+if (ingredients.includes(`spinach`)) clearTimeout(pizzaTimer);
+*/
+
+// setInterval function
+/*
+setInterval(() => console.log(new Date()), 5000);
+*/
