61cdaa5eda4bf1c91b41d61a8118843450e7cab8
DimaD1988 <dimdon88@gmail.com>
2023-06-24 10:12:17 +0700

    feat: add OOP exersice-1 js file

---
+++ Lessons/12-OOP/exersice-1.js
@@ -0,0 +1,26 @@
+'use strict';
+/*
+const Car = function (make, speed) {
+  this.make = make;
+  this.speed = speed;
+};
+
+Car.prototype.accelerate = function () {
+  this.speed += 10;
+};
+
+Car.prototype.brake = function () {
+  this.speed -= 5;
+};
+
+const bmw = new Car(`BMW`, 120);
+const mercedes = new Car(`Mercedes`, 95);
+console.log(bmw, mercedes);
+bmw.brake();
+bmw.brake();
+bmw.brake();
+bmw.brake();
+bmw.brake();
+
+console.log(bmw, mercedes);
+*/
