f088b501ea882815d76daedc3680a805832426a2
DimaD1988 <dimdon88@gmail.com>
2023-07-05 16:05:33 +0700

    feat: add OOP exersice-9 js file

---
+++ Lessons/12-OOP/exersice-9.js
@@ -0,0 +1,39 @@
+'use strict';
+/*
+class Pet {
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+  }
+
+  eat() {
+    return `${this.name} is eating!`;
+  }
+}
+
+class Cat extends Pet {
+  constructor(name, age, livesLeft = 9) {
+    super(name, age);
+    this.livesLeft = livesLeft;
+  }
+  meow() {
+    return 'MUOR-MEOWWW!!!';
+  }
+}
+
+class Dog extends Pet {
+  bark() {
+    return 'WOOF-WOOF!!!';
+  }
+
+  eat() {
+    return `${this.name} scarfs his food!`;
+  }
+}
+
+const tom = new Cat('Tom', 1);
+const barkey = new Dog('Barkey', 3);
+console.log(tom, barkey);
+console.log(tom.eat(), tom.meow());
+console.log(barkey.eat());
+*/
