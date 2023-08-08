9c437c9c43cedb4c1707889afaa4dc6b75fcd56e
DimaD1988 <dimdon88@gmail.com>
2023-08-08 13:24:13 +0700

    feat: add Intro-to-Node-JS index.js file

---
+++ Lessons/15-Small Projects/1 - Intro to Node JS/Example/index.js
@@ -0,0 +1,7 @@
+const counterObject = require('./myscript');
+console.log(counterObject.getCounter()); // 0
+counterObject.incrementCounter();
+console.log(counterObject.getCounter()); // 1
+
+const newCounterObject = require('./myscript');
+console.log(newCounterObject.getCounter()); // 1
