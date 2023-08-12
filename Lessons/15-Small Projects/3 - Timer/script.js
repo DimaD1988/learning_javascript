94e286342cd4f8d028b001209fe60d3778a84fda
DimaD1988 <dimdon88@gmail.com>
2023-08-12 14:51:51 +0700

    feat: add Timer script.js file

---
+++ Lessons/15-Small Projects/3 - Timer/script.js
@@ -0,0 +1,26 @@
+"use strict";
+
+const durationInput = document.querySelector("#duration");
+const startBtn = document.querySelector("#start");
+const pauseBtn = document.querySelector("#pause");
+const circle = document.querySelector("circle");
+
+const perimeter = circle.getAttribute("r") * 2 * Math.PI;
+circle.setAttribute("stroke-dasharray", perimeter);
+let currOffset = 0;
+let duration;
+
+const timer = new Timer(durationInput, startBtn, pauseBtn, {
+	onStart(totalDuration) {
+		duration = totalDuration;
+	},
+	onTick(timeRemaining) {
+		circle.setAttribute(
+			"stroke-dashoffset",
+			(perimeter * timeRemaining) / duration - perimeter
+		);
+	},
+	onComplete() {
+		console.log("Timer Completed");
+	},
+});
